62466cd666df44712fef65368270ff9b93075855
Veta322 <vetarejh@gmail.com>
2023-05-12 16:43:36 +0300

    fix api2

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -90,7 +90,7 @@ _checkResponse(res) {
 }
 
 const api = new Api({
-    url: "http://api.domain.veta.nomoredomains.monster",
+    url: "api.domain.veta.nomoredomains.monster",
     headers: {
         Authorization: `Bearer ${localStorage.getItem('jwt')}`,
         "Content-Type": "application/json",
